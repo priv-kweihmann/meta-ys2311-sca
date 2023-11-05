a82f20bb492afe12ebb58d28fa6b6377e977dccf
Konrad Weihmann <kweihmann@outlook.com>
2023-11-05 11:43:56 +0000

    docker: make entrypoint configurable
    Signed-off-by: Konrad Weihmann <kweihmann@outlook.com>

--- docker/build.sh
+++ docker/build.sh
@@ -4,17 +4,22 @@
 
 set -e
 
-if [ "$#" != 2 ]; then
-    echo "Usage: $(basename "${0}") <image-tag> <sdk-install-script.sh>"
+if [ "$#" -lt 2 ]; then
+    echo "Usage: $(basename "${0}") <image-tag> <sdk-install-script.sh> [entry-point]"
     exit
 fi
 
 TAG="${1}"
 SDK="${2}"
+if [ "$#" -gt 2 ]; then
+    ENTRY="${3}"
+else
+    ENTRY="entry.sh"
+fi
 
 DIR=$(dirname "${0}")
 cp "${SDK}" "${DIR}/sdk.sh"
 
-docker build -t "${TAG}" --build-arg="SDKINSTALLER=${DIR}/sdk.sh" .
+docker build -t "${TAG}" --build-arg="SDKINSTALLER=${DIR}/sdk.sh" --build-arg="ENTRY=$ENTRY" .
 
 rm "${DIR}/sdk.sh"
